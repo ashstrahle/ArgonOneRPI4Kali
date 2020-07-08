21c4af60ede3168b0429b3cdd6f0da71efb5cac9
Ashley Strahle <a.strahle@me.com>
2020-07-08 10:12:58 +1000

    reformat

--- argononed.py
+++ argononed.py
@@ -32,6 +32,8 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 shutdown_pin=4
 GPIO.setup(shutdown_pin, GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
+
+
 def shutdown_check():
 	while True:
 		pulsetime = 1
